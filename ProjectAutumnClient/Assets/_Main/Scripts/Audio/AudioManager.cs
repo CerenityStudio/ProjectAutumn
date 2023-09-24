4bc94b30ab21cd51dccdebc8db0dfbbb77761542
Murran <claytoo.serophina@gmail.com>
2023-09-24 12:49:29 +0700

    Add Basic Input System

---
+++ ProjectAutumnClient/Assets/_Main/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+namespace CerenityStudio
+{
+    public class AudioManager : MonoBehaviour
+    {
+        public static AudioManager instance { get; private set; }
+
+    #region VARIABLE
+        // [field: Header("Music")]
+        // [field: SerializeField] public EventReference bgm { get; private set; }
+
+        // [field: Header("SFX")]
+        // [field: SerializeField] public EventReference jumpSound { get; private set; }
+    #endregion
+
+    #region UNITY FUNCTION
+        private void Awake()
+        {
+            if (instance != null)
+            {
+#if UNITY_EDITOR
+                Debug.LogError("Found more than one Audio Manager in the scene");
+#endif
+            }
+            instance = this;
+        }
+
+        // void Start()
+        // {
+        //     PlayerController.onBump += PlayJumpSound;
+        // }
+
+        // private void OnDestroy()
+        // {
+        //     PlayerController.onBump -= PlayJumpSound;
+        // }
+    #endregion
+
+    #region PUBLIC FUNTION
+        // public void PlayOneShot(EventReference sound, Vector3 worldPos)
+        // {
+        //     RuntimeManager.PlayOneShot(sound, worldPos);
+        // }
+
+        // public void PlayBumpSound()
+        // {
+        //     //AudioManager.instance.PlayOneShot(FMODEvents.instance.bumpSound, this.transform.position);
+        //     PlayOneShot(bumpSound, this.transform.position);
+        // }
+    #endregion
+
+    #region PRIVATE FUNCTION
+    #endregion
+    }
+}
---
+++ ProjectAutumnClient/Assets/_Main/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace CerenityStudio
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+    #region VARIABLE
+        public Transform groundCheck;
+        public LayerMask groundLayer;
+        public Animator anim;
+
+        private Rigidbody2D rb;
+        private float horizontal;
+        [SerializeField] private float speed = 5f;
+        [SerializeField] private float jumpingPower = 16f;
+        [SerializeField] private bool isFacingRight = true;
+    #endregion
+
+    #region UNITY FUNCTION
+        // Start is called before the first frame update
+        void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+
+            if(!isFacingRight && horizontal > 0f)
+            {
+                Flip();
+            }
+            else if (isFacingRight && horizontal < 0f)
+            {
+                Flip();
+            }
+        }
+    #endregion
+
+    #region PUBLIC FUNCTION
+        // input ketika bergerak moving
+        public void Move(InputAction.CallbackContext context)
+        {
+            horizontal = context.ReadValue<Vector2>().x;
+
+            // ketika salah satu tombol WASD ditekan
+            if (context.performed)
+            {
+                anim.SetBool("isMove", true);
+            }
+
+            // ketika salah satu tombol WASD dilepas
+            if (context.canceled)
+            {
+                anim.SetBool("isMove", false);
+            }
+        }
+
+        // input ketika lompat
+        public void Jump(InputAction.CallbackContext context)
+        {
+            // ketika tombol lompat ditekan
+            if (context.performed && IsGrounded())
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+                anim.SetTrigger("isJump");
+            }
+
+            // ketika tombol lompat dilepas
+            if (context.canceled && rb.velocity.y > 0f)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+            }
+        }
+    #endregion
+
+    #region PRIVATE FUNCTION
+        private bool IsGrounded()
+        {
+            return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        }
+
+        private void Flip()
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    #endregion
+    }
+}
